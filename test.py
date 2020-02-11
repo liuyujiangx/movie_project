13f023d34d6bb5337088fbed62ffcd540ac8830c
liuyujiangx <1914571065@qq.com>
2020-02-11 13:41:00 +0800

    gx

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1 style='color:red'>hello word</h1>"
+    return "<h1 style='color:red'>hello word！！！！！</h1>"
 
 if __name__ =="__main__":
     app.run()
