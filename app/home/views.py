fe1fd3e902c738c32fd41c95677f25cc1b1cefea
liuyujiangx <1914571065@qq.com>
2020-03-26 19:20:59 +0800

    gx

--- app/home/views.py
+++ app/home/views.py
@@ -235,7 +235,8 @@ def mycb():
     body = {'grant_type':'authorization_code','client_id':'101860781',
             'client_secret':'0f5a014e13e7d35fbcca51ecc2ff6745','code':code,'redirect_uri':'https://yujl.top/mycb'}
     response=requests.get(url,params=body)
-    token = response.text
+    response.content.decode("unicode_escape")
+    token = response.json()
     print(token)
     return render_template("home/mycb.html")
 
